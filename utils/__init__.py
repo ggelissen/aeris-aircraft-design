668c6276c9ccce698a58e6959755ac1512898795
ggelissen <g.m.j.gelissen@student.tudelft.nl>
2025-05-26 11:21:00 +0200

    Moved .gitignore file

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+# utils/__init__.py
+# This file is empty on purpose. Its presence makes 'utils' a package, from which functions or classes can be imported.
+# In case you want direct access to the functions within utils files, you can import them here.
