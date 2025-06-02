39203efbf40daeae17afcb17916c40b161905d05
eliseterwogt <eliseterwogt@gmail.com>
2025-06-02 08:54:10 +0200

    landing gear

--- class2/undercarriage.py
+++ class2/undercarriage.py
@@ -11,7 +11,8 @@ from utils.unit_conversions import *
 def undercarriage_sizing(params:DesignParameters, surface_type='paved'):
     W_TO = params.weight.W_TO
     LCN = 40 # Load Classification Number, typical for light aircraft
-    tire_pressure = (430*math.log(LCN) -680)*1000 # in kPa
+    tire_pressure = (430*math.log(LCN) -680)*1000 # in Pa
+    tire_pressure_kgcm2 = tire_pressure / 98070
     static_nose_fraction = 0.08
     static_main_fraction = 1 - static_nose_fraction
     W_nose = static_nose_fraction * W_TO
@@ -21,27 +22,11 @@ def undercarriage_sizing(params:DesignParameters, surface_type='paved'):
 
     static_load_main = W_main_total / num_main_wheels
     static_load_nose = W_nose / num_nose_wheels
+    static_load_main_kg = static_load_main / 9.81  # Convert to kg
+    static_load_nose_kg = static_load_nose / 9.81  # Convert to kg
+    print(f"Static Load on Nose Wheel: {static_load_nose_kg:.2f} kg")
+    print(f"Static Load on Main Wheels: {static_load_main_kg:.2f} kg")
+    print(f"Tire Pressure: {tire_pressure_kgcm2:.2f} kg/cm^2")
 
-    print(f"Static Load on Nose Wheel: {static_load_nose:.2f} N")
-    print(f"Static Load on Main Wheels: {static_load_main:.2f} N")
-
-    def estimate_tire_diameter(laod_N, pressure_Pa):
-        load_kg = load_N / G
-        pressure_bar = pressure_Pa / 100000  # Convert Pa to bar
-        return 0.5* (load_kg / pressure_bar) ** (0.25)
+def undercarriage_positioning(params: DesignParameters):
     
-    main_diameter_m = estimate_tire_diameter(static_load_main, tire_pressure)
-    nose_diameter_m = estimate_tire_diameter(static_load_nose, tire_pressure)
-
-    print(f"Estimated Main Tire Diameter: {main_diameter_m:.2f} m")
-    print(f"Estimated Nose Tire Diameter: {nose_diameter_m:.2f} m")
-
-    return {
-        'tire_pressure': tire_pressure,
-        'static_load_nose': static_load_nose,
-        'static_load_main': static_load_main,
-        'main_diameter_m': main_diameter_m,
-        'nose_diameter_m': nose_diameter_m,
-        'num_main_wheels': num_main_wheels,
-        'num_nose_wheels': num_nose_wheels
-    }
