40b43f41585ba911016f52e1a43142e2dd01ac16
L.C. Huirne <l.huirne@outlook.com>
2025-06-04 16:40:20 +0200

    Added some functions

--- design_variables.py
+++ design_variables.py
@@ -195,8 +195,8 @@ class WingParameters:
         self.yehudi = True
         self.yehudi_pos_frac = 0.3 # Yehudi Position Fraction, where 0 is the root and 1 is the tip
         self.yehudi_area = 6.0 # Yehudi area m2
-        self.yehudi_flaps = FlapGroup(spanwise_pos_frac_inbound=0.1, spanwise_pos_frac_outbound=0.2, flapwidth=0.4)
-        self.main_flaps = FlapGroup(spanwise_pos_frac_inbound=0.25, spanwise_pos_frac_outbound=0.5, flapwidth=0.4)
+        self.yehudi_flaps = FlapGroup(spanwise_pos_frac_inbound=0.11, spanwise_pos_frac_outbound=0.3, flapwidth=0.4)
+        self.main_flaps = FlapGroup(spanwise_pos_frac_inbound=0.35, spanwise_pos_frac_outbound=0.7, flapwidth=0.4)
         self.flapgroups = [self.yehudi_flaps, self.main_flaps]
 
         
--- subsystems/structures/wing_structure_generation.py
+++ subsystems/structures/wing_structure_generation.py
@@ -110,7 +110,7 @@ def cross_sectional_structure_along_span(designvars: DesignParameters = None, sp
 
     # make room for flaps:
     for flapgroup in designvars.wing.flapgroups:
-        if spanwise_position*designvars.wing.b_w/2 - flapgroup.spanwise_pos_frac_inbound > 0.0 and spanwise_position*designvars.wing.b_w/2 - flapgroup.spanwise_pos_frac_outbound < 0.0:
+        if spanwise_position - flapgroup.spanwise_pos_frac_inbound > 0.0 and spanwise_position - flapgroup.spanwise_pos_frac_outbound < 0.0:
             trailing_edge_position = outline[np.argmax(outline[:, 0])][0]
             cut_out_length_from_trailing_edge = flapgroup.flapwidth
             cut_out_position_x = trailing_edge_position -  cut_out_length_from_trailing_edge
