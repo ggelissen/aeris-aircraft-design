8db1df081ba19d5d3f17b120c4d0fbb5d802f9ef
Luuk Scheffer <l.s.scheffer@student.tudelft.nl>
2025-06-04 14:51:45 +0200

    datcom fix

--- subsystems/flightperformance/datcom.py
+++ subsystems/flightperformance/datcom.py
@@ -1,5 +1,7 @@
 import os
 from subprocess import Popen, PIPE
 
-p = Popen('C:\\Users\\Luuk\\Documents\\DSEGITHUB\\DSEGroup17\\subsystems\\flightperformance\\datcom.exe', stdin=PIPE) #NOTE: no shell=True here
-#p.communicate(os.linesep.join(["aeris.INP"]))
+path = os.path.dirname(os.path.abspath(__file__))
+
+p = Popen(f'{path}\\datcom.exe', stdin=PIPE) #NOTE: no shell=True here
+p.communicate(str.encode(f"{path}\\aeris.INP"))
