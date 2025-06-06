08db6e4a4573057dabb484514807bb6ecbde8b96
esanches5700310 <e.w.sanches@student.tudelft.nl>
2025-06-06 15:31:40 +0200

    save

--- design_variables.py
+++ design_variables.py
@@ -2,7 +2,7 @@ import math as m
 import numpy as np
 import yaml
 
-from loading_diagrams.py import WingLoadingDiagrams()
+
 
 
 class DesignParameters:
@@ -213,8 +213,6 @@ class WingParameters:
         self.k2 = 1 / (np.pi * self.A_w_target * self.e)
 
 
-        # Internal Loading Diagrams
-        self.shear_z, self.bend_moment_x, self.torsion_y, self.shear_x, self.bend_moment_z = WingLoadingDiagrams().run_analysis_for_case(PLOT=False)
 
         
 
