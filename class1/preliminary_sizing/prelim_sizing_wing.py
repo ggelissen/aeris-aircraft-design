2ac7018855be0163f33f12940ef035c00f82bf6f
Alejandro Trigueros Alonso <a.triguerosalonso@student.tudelft.nl>
2025-06-14 14:14:27 +0200

    Added PJ Mdd sweep equation, from his optimal climate aircraft paper w Vos

--- class1/preliminary_sizing/prelim_sizing_wing.py
+++ class1/preliminary_sizing/prelim_sizing_wing.py
@@ -26,7 +26,7 @@ def calculate_sweep_angle_025c_rad(Mach_cruise: float, Mach_cross: float = 0.935
         return np.arccos(1)
     else:
         #return np.arccos(0.75 * (Mach_cross / Mach_dd))
-        np.arccos(((1.16)  / Mach_dd + 0.5))
+        np.arccos(1.16  / (Mach_dd + 0.5)) # TODO, equation from PJ thesis
     
 
 def calculate_taper_ratio(Lambda_025c: float) -> float:
@@ -109,6 +109,7 @@ def calculate_thickness_ratio(h: float, Mach_cruise: float, W_TO: float, S: floa
     C_L = W_TO / (q * S)
     Mach_dd = Mach_cruise + 0.03
 
+    # Calculate thickness-to-chord ratio using the Korn-Lock formula for Mdd
     t_c = min(((np.cos(Lambda_05c))**3 * (Mach_cross - Mach_dd * np.cos(Lambda_05c)) - 0.115 * C_L**(1.5))/((np.cos(Lambda_05c))**2), 0.18)
     return t_c
 
